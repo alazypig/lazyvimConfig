b3b0f4ffc1847887c32085b994e3e1cbb5c967c7
Edward Wang <wang.huiyang@outlook.com>
2024-07-11 15:21:18 +0800

    feat: use mini-comment plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,13 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "plugins" },
   },
   defaults = {
