1fcb57024556ddaf31ff4f1147a53caf1417b678
Edward Wang <wang.huiyang@outlook.com>
2025-01-03 17:15:03 +0800

    feat(plugin): add flutter plugin

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("flutter-tools").setup({
+  lsp = {
+    color = {
+      enabled = true,
+      background = true,
+    },
+  },
+})
