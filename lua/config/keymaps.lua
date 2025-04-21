ab8e22cab019df1555176840ea6590dfe71a2967
Edward Wang <wang.huiyang@outlook.com>
2025-04-21 11:55:23 +0800

    fix: disable 'zz' fold wrap on vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,13 +5,15 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<Esc>")
 keymap.set("n", "zz", function()
-  local line = vim.fn.line(".")
-  local foldclosed = vim.fn.foldclosed(line)
+  if not vim.g.vscode then
+    local line = vim.fn.line(".")
+    local foldclosed = vim.fn.foldclosed(line)
 
-  if foldclosed == -1 then
-    vim.cmd("normal! zc")
-  else
-    vim.cmd("normal! zo")
+    if foldclosed == -1 then
+      vim.cmd("normal! zc")
+    else
+      vim.cmd("normal! zo")
+    end
   end
 end, { desc = "Toggle fold under cursor" })
 
@@ -21,4 +23,3 @@ if not vim.g.vscode then
     require("fittencode").accept_all_suggestions()
   end)
 end
-
