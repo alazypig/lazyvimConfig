4bc99f2576b40380c270336f50a3d3bc29e55823
Edward Wang <wang.huiyang@outlook.com>
2025-05-08 15:46:40 +0800

    fix: fix mason 2.0

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,22 +19,22 @@ require("lazy").setup({
   spec = {
     -- LazyVim 核心插件
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    
+
     -- 导入额外功能模块
     { import = "lazyvim.plugins.extras.formatting.prettier" }, -- Prettier 格式化支持
-    { import = "lazyvim.plugins.extras.lang.typescript" },     -- TypeScript 支持
-    { import = "lazyvim.plugins.extras.lang.json" },          -- JSON 支持
-    { import = "lazyvim.plugins.extras.lang.markdown" },       -- Markdown 支持
-    { import = "lazyvim.plugins.extras.lang.toml" },          -- TOML 支持
-    { import = "lazyvim.plugins.extras.lang.tailwind" },      -- Tailwind CSS 支持
-    { import = "lazyvim.plugins.extras.lang.vue" },           -- Vue.js 支持
-    { import = "lazyvim.plugins.extras.lang.yaml" },          -- YAML 支持
+    { import = "lazyvim.plugins.extras.lang.typescript" }, -- TypeScript 支持
+    { import = "lazyvim.plugins.extras.lang.json" }, -- JSON 支持
+    { import = "lazyvim.plugins.extras.lang.markdown" }, -- Markdown 支持
+    { import = "lazyvim.plugins.extras.lang.toml" }, -- TOML 支持
+    { import = "lazyvim.plugins.extras.lang.tailwind" }, -- Tailwind CSS 支持
+    { import = "lazyvim.plugins.extras.lang.vue" }, -- Vue.js 支持
+    { import = "lazyvim.plugins.extras.lang.yaml" }, -- YAML 支持
     { import = "lazyvim.plugins.extras.coding.mini-comment" }, -- 注释增强
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },    -- 启动页面
-    { import = "plugins" },                                    -- 自定义插件配置
+    { import = "lazyvim.plugins.extras.ui.mini-starter" }, -- 启动页面
+    { import = "plugins" }, -- 自定义插件配置
   },
   defaults = {
-    lazy = false,    -- 默认不延迟加载自定义插件
+    lazy = false, -- 默认不延迟加载自定义插件
     version = false, -- 使用最新的 git commit
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
