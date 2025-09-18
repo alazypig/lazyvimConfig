f4bc5b0455cb1a1d6c7b6f6141037f03e2121b75
Edward Wang <wang.huiyang@outlook.com>
2025-09-18 09:36:15 +0800

    feat: update mason plugin

--- lua/plugins/mason-workaround.lua
+++ lua/plugins/mason-workaround.lua
@@ -1,4 +1,20 @@
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = {
+      -- 确保安装以下语言服务器和代码检查工具
+      ensure_installed = {
+        -- 代码格式化工具
+        "stylua", -- Lua 代码格式化
+        "prettier", -- 通用代码格式化（支持 JS/TS/CSS/HTML 等）
+        "shfmt", -- Shell 脚本格式化
+
+        -- 代码检查工具
+        "shellcheck", -- Shell 脚本检查
+        "flake8", -- Python 代码检查
+      },
+    },
+  },
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
